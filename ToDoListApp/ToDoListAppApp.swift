bda93e6f1f0e0d61e450201264fbbeff5617269e
Alaina Varghese <alainavarghese18@gmail.com>
2023-06-14 11:19:01 -0400

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Susan Varghese on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
